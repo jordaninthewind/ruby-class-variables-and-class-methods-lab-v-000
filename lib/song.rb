3dfa861790e5bc091a207fc861f4c2282fd0ea33
Jordan Kline <jordan.kline@gmail.com>
2018-01-08 17:14:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,14 @@ class Song
 attr_reader :name, :artist, :genre
 
 @@count = 0
+@@artists = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists << artist
   end
 
 
